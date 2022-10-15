eb78684c0f07bb067ffc3f42b37e9d487af654a4
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-10-15 11:49:59 -0500

    saturday workshop 10.15

--- authenticate.js
+++ authenticate.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
@@ -38,3 +38,14 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    console.log(req)
+    if(req.user.admin){
+        return next();
+    }else{
+        const err = new Error('You are not an admin');
+        err.status = 403;
+        return next(err);
+    }
+}
